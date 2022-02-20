d151749ba3c57aaa229afce443a3a7ce6696779f
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-20 14:23:07 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -13,7 +13,6 @@ async def unauth(_, m: Message):
  if m.from_user.id not in Var.AUTH_USERS:
   await m.reply_text("You are not Authorized User, Msg Me To Be Auth Users")
  if m.from_user.id in Var.AUTH_USERS:
-async def start(c, m: Message):
   await m.reply_text(
         f'**Hi {m.from_user.mention(style="md")},\nSend or Forward Me Any File Here Which You Want To Upload Remotely In Streaam.net.**'
     )
