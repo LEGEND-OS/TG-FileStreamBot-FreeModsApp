abe6b2b526fdc251b4d4fbad32bbba4bb45f9856
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-20 14:12:40 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,39 +7,6 @@ from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
 from pyrogram.errors import UserNotParticipant
 
-@StreamBot.on_message(filters.command(["start", "help"]))
-async def start(c, m: Message):
-    if Var.UPDATES_CHANNEL != "None":
-        try:
-            user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-            if user.status in ["kicked", "banned"]:
-                await c.send_message(
-                    chat_id=m.chat.id,
-                    text="__You are Banned in Our Updates Channel ❌__\n\n  **Contact @LegendAkshay**",
-                    parse_mode="markdown",
-                    disable_web_page_preview=True
-                )
-                return
-        except UserNotParticipant:
-            await c.send_message(
-                chat_id=m.chat.id,
-                text="""<i>This Is a Private Bot For our Streaam.net Users, You have to Join Our Telegram Channel To Verify You</i>""",
-                reply_markup=InlineKeyboardMarkup(
-                    [[ InlineKeyboardButton("JOIN", url=f"https://t.me/{Var.UPDATES_CHANNEL}") ]]
-                ),
-                parse_mode="HTML"
-            )
-            return
-        except Exception:
-            await c.send_message(
-                chat_id=m.chat.id,
-                text="**Something Went Wrong Contact Dev** @LegendAkshay",
-                parse_mode="markdown",
-                disable_web_page_preview=True)
-            return
-    await m.reply(
-        f'**Hi {m.from_user.mention(style="md")},\nSend or Forward Me Any File Here Which You Want To Upload Remotely In Streaam.net.**'
-    )
 
 @StreamBot.on_message(filters.private)
 async def unauth(_, m: Message):
