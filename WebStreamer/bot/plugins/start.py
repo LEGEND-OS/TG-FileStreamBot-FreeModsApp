b3d2904d344cae19bef6726f275d8203c160f6d9
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-20 18:59:45 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -5,14 +5,10 @@ from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
-from pyrogram.errors import UserNotParticipant
 
 
-@StreamBot.on_message(filters.private & filters.command(["start", "help"]))
+@StreamBot.on_message(filters.command(["start", "help"]))
 async def unauth(_, m: Message):
- if m.from_user.id not in Var.AUTH_USERS:
-  await m.reply_text("**This Bot is Only For Authorised Users of Streaam.net**\n\n__Contact @LegendAkshay Or @SID12O__")
- if m.from_user.id in Var.AUTH_USERS:
   await m.reply_text(
         f'**Hi {m.from_user.mention(style="md")},\nSend or Forward Me Any File Here Which You Want To Upload Remotely In Streaam.net.**'
     )
