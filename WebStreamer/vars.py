a0df0f65d4ca928678737279930ede4f556539e7
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-20 13:51:42 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -13,13 +13,14 @@ class Var(object):
     API_HASH = str(environ.get("API_HASH"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
     UPDATES_CHANNEL = str(environ.get('UPDATES_CHANNEL', None))
-    AUTH_USER = list(set(int(x) for x in str(environ.get("AUTH_USER")).split()))
+    ADMIN = list(set(int(x) for x in str(environ.get("AUTH_USER")).split()))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(
         environ.get("BIN_CHANNEL", None)
     )  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
     PORT = int(environ.get("PORT", 8080))
+    AUTH_USERS = [2018327763]
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     HAS_SSL = environ.get("HAS_SSL", False)
