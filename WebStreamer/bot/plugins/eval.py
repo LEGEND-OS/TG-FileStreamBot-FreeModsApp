1f2854d5607b3f65c5bbcd723d1d3b6690a85e2b
Legend Akshay <88607836+FreeModsApp@users.noreply.github.com>
2022-02-20 12:34:25 +0530

    Update eval.py

--- WebStreamer/bot/plugins/eval.py
+++ WebStreamer/bot/plugins/eval.py
@@ -9,6 +9,9 @@ import sys
 import time
 import traceback
 
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
 from WebStreamer.__main__ import * # yea 
 from logging.handlers import RotatingFileHandler
