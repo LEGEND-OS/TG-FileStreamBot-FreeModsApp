bec8df3cb2b44a9385cbbbadcfffd0f1affd3e1e
DeekshithSH <shdeekshith@gmail.com>
2022-02-21 12:55:28 +0530

    Improved

--- WebStreamer/utils/onstreaam.py
+++ WebStreamer/utils/onstreaam.py
@@ -1,12 +1,11 @@
 import aiohttp
 
-from urllib3 import Retry
-
 async def user_in_streaam(id: int):
-
     async with aiohttp.ClientSession() as session:
-        url = f'https://streaam.net/dash/check.php?id={id}'
-        async with session.get(url) as resp:
+        url = f'https://streaam.net/dash/check.php'
+        params = {'id': id}
+
+        async with session.get(url, params=params) as resp:
             text_resp = await resp.text()
             if text_resp == 'ok':
                 return True
